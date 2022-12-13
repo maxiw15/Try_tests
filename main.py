b73b19f4da65386812dada609f6c586181df979b
max.iw15 <max.iw15@yandex.ru>
2022-12-13 16:27:11 +0300

    finish

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import requests
+from TOKEN_secret import token
+
+
 def task_1(geo_logs: list) -> list:
     answer = []
     for dicts in geo_logs:
@@ -21,3 +25,22 @@ def task_3(stats: dict) -> list or int:
         if element[1] == max(stats.values()):
             answer.append(element[0])
     return answer
+
+
+class YandexDisk:
+
+    def __init__(self, token):
+        self.token = token
+
+    def create_folder(self, name_folder):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources?"
+        headers = {"Authorization": self.token}
+        params = {"path": name_folder}
+        response = requests.put(upload_url, headers=headers, params=params)
+        return response.status_code
+
+    def exist_folder(self):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/files"
+        headers = {"Authorization": self.token}
+        response = requests.get(upload_url, headers=headers)
+        return response.text
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
-from main import task_1, task_2, task_3
+from main import task_1, task_2, task_3, YandexDisk
+from TOKEN_secret import token
 
 
 class TestDrive(unittest.TestCase):
@@ -56,6 +57,34 @@ class TestDrive(unittest.TestCase):
         for i in task_1(run):
             self.assertEqual(list(i.values())[0][1], "Россия")
             self.assertTrue(list(i.values())[0][0])
+
+    def test_task_2(self):
+        ids = {'user1': [213, 213, 213, 15, 213],
+               'user2': [54, 54, 119, 119, 119],
+               'user3': [213, 98, 98, 35]}
+        res = task_2(ids)
+        self.assertEqual(res, [98, 35, 15, 213, 54, 119])
+        ids = {'user1': [],
+               'user2': [],
+               'user3': []}
+        res = task_2(ids)
+        self.assertEqual(res, [])
+
+    def test_task_3(self):
+        stats = {'facebook': 120, 'yandex': 160, 'vk': 115, 'google': 99, 'email': 42, 'ok': 98}
+        res = task_3(stats)
+        self.assertEqual(res, ['yandex'])
+        stats = {'facebook': 160, 'yandex': 160, 'vk': 115, 'google': 99, 'email': 42, 'ok': 98}
+        res = task_3(stats)
+        self.assertEqual(res, ['facebook', 'yandex'])
+
+    def test_yandex_folder_create(self):
+        test_yandex = YandexDisk(token)
+        res = test_yandex.create_folder(name_folder="test")
+        # self.assertEqual(res, 201)
+        res = test_yandex.exist_folder()
+        self.assertIn("test", res)
+
 
 
 if __name__ == '__main__':
